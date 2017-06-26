ef075d47d9ee5dc0948cb2b49da5ffd5d8349f26
ckhurana000 <ckhurana000@gmail.com>
2017-06-26 06:06:55 +0530

    Initial Commit

---
+++ media.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+class Movie():
+
+	# poster  stores url of the poster image for the movie
+	# trailer  stores url of the youtube trailer video
+	def __init__(self, title, story, poster, trailer):
+		self.title = title
+		self.story = story
+		self.poster = poster
+		self.trailer = trailer
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer)
